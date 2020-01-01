3f9f5d8145f6ef5914ddb2fabff324d903968226
Jeongsu Yang <mycat83@gmail.com>
2020-01-01 11:14:53 +0900

    기능: 코딩테스트 연습
    * 해시 - 전화번호 목록

---
+++ src/hash/phonenumber/Solution.java
@@ -0,0 +1,20 @@
+package hash.phonenumber;
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i].startsWith(phone_book[i + 1]) || phone_book[i + 1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
