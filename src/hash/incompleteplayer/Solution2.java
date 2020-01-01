85f2195e56c426dcea65f8ead123a40a7365450c
Jeongsu Yang <mycat83@gmail.com>
2020-01-01 10:42:22 +0900

    기능: 코딩테스트 연습
    * 해시 - 완주하지 못한 선수

---
+++ src/hash/incompleteplayer/Solution2.java
@@ -0,0 +1,27 @@
+package hash.incompleteplayer;
+
+import java.util.Arrays;
+
+class Solution2 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int participantIndex = 0;
+        for (String player: completion) {
+            if (!player.equals(participant[participantIndex])) {
+                answer = participant[participantIndex];
+                break;
+            }
+            participantIndex++;
+        }
+
+        if (participantIndex < participant.length) {
+            answer = participant[participantIndex];
+        }
+
+        return answer;
+    }
+}
