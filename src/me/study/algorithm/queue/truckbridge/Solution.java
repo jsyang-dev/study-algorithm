6a2425b4d9bdfd3c515990db8ed9cca6e53f91a7
Jeongsu Yang <mycat83@gmail.com>
2020-01-03 22:17:55 +0900

    기능: 코딩테스트 연습
    * 큐 - 다리를 지나는 트럭

---
+++ src/me/study/algorithm/queue/truckbridge/Solution.java
@@ -0,0 +1,32 @@
+package me.study.algorithm.queue.truckbridge;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 1;
+        Queue<Integer> queue = new LinkedList<>();
+
+        int index = 1;
+        int truckWeightsSum = truck_weights[0];
+        queue.offer(truck_weights[0]);
+        while (truckWeightsSum > 0) {
+            if (queue.size() == bridge_length) {
+                truckWeightsSum -= queue.poll();
+            }
+
+            if (index < truck_weights.length && truckWeightsSum + truck_weights[index] <= weight) {
+                queue.offer(truck_weights[index]);
+                truckWeightsSum += truck_weights[index];
+                index++;
+            } else {
+                queue.offer(0);
+            }
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
