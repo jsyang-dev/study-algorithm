5509568971c3f3d10e70e6eaee2cd7c044e0430c
Jeongsu Yang <mycat83@gmail.com>
2020-01-06 15:35:25 +0900

    기능: 코딩테스트 연습
    * 스택 - 쇠막대기

---
+++ src/me/study/algorithm/stack/ironrod/Solution.java
@@ -0,0 +1,22 @@
+package me.study.algorithm.stack.ironrod;
+
+class Solution {
+    public int solution(String arrangement) {
+        int answer = 0;
+        int bracket = 0;
+        arrangement = arrangement.replace("()", "L");
+
+        for (int i = 0; i < arrangement.length(); i++) {
+            if (arrangement.charAt(i) == '(') {
+                bracket++;
+            } else if (arrangement.charAt(i) == ')') {
+                bracket--;
+                answer++;
+            } else if (arrangement.charAt(i) == 'L') {
+                answer += bracket;
+            }
+        }
+
+        return answer;
+    }
+}
