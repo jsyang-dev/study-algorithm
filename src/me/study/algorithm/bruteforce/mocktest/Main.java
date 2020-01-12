d9b9254a910d6232d7f32a2113cb2913a62299cb
Jeongsu Yang <mycat83@gmail.com>
2020-01-12 12:08:38 +0900

    기능: 코딩테스트 연습
    * 완전탐색 - 모의고사

---
+++ src/me/study/algorithm/bruteforce/mocktest/Main.java
@@ -0,0 +1,22 @@
+package me.study.algorithm.bruteforce.mocktest;
+
+import java.util.Arrays;
+
+/**
+ * [모의고사]
+ * 시험은 최대 10,000 문제로 구성되어있습니다.
+ * 문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+ * 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+ */
+class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] answers;
+
+        answers = new int[] {1,2,3,4,5};
+        System.out.println(Arrays.toString(solution.solution(answers)));
+
+        answers = new int[] {1,3,2,4,2};
+        System.out.println(Arrays.toString(solution.solution(answers)));
+    }
+}
---
+++ src/me/study/algorithm/bruteforce/mocktest/Solution.java
@@ -0,0 +1,33 @@
+package me.study.algorithm.bruteforce.mocktest;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = new int[3];
+        int[][] pattern = new int[][] {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] correct = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                int index = i % pattern[j].length;
+                if (answers[i] == pattern[j][index]) {
+                    correct[j]++;
+                }
+            }
+        }
+
+        OptionalInt max = Arrays.stream(correct).max();
+        int index = 0;
+        for (int i = 0; i < correct.length; i++) {
+            if (correct[i] == max.getAsInt()) {
+                answer[index] = i + 1;
+                index++;
+            }
+        }
+
+        return Arrays.stream(answer).filter(x -> x > 0).toArray();
+    }
+}
+
