95441bc8d1d07157ae3b914cc1d2e1c516445802
Jeongsu Yang <mycat83@gmail.com>
2020-02-08 11:15:37 +0900

    기능: 코딩테스트 연습 - Arrays
    * HackerRank - New Year Chaos

--- src/me/study/algorithm/hackerrank/arrays/newyearchaos/Solution.java
+++ src/me/study/algorithm/hackerrank/arrays/newyearchaos/Solution.java
@@ -14,33 +14,53 @@ class Solution {
             arr[i] = i + 1;
         }
 
-        for (int i = 0; i < q.length; i++) {
-            int start = Math.max(i - 2, 0);
-            int end = Math.min(i + 2, q.length - 1);
-            for (int j = start; j < i; j++) {
-                int left = map.getOrDefault(arr[j], 0);
-                int right = map.getOrDefault(arr[j + 1], 0);
-                if (arr[j] == q[i] && left < 2 && right < 2) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        int action = -1;
+        while (action != 0) {
+            action = 0;
+            for (int i = 0; i < q.length; i++) {
+                int start = Math.max(i - 2, 0);
+                int end = Math.min(i + 2, q.length - 1);
 
-                    map.put(arr[j], right + 1);
-                    map.put(arr[j + 1], left + 1);
-                    result++;
+                for (int j = start; j < i; j++) {
+                    int left = map.getOrDefault(arr[j], 0);
+                    int right = map.getOrDefault(arr[j + 1], 0);
+                    if (arr[j] == q[i]) {
+                        if (arr[j] > arr[j + 1] && left < 2) {
+                            map.put(arr[j], left + 1);
+                        } else if (arr[j] < arr[j + 1] && right < 2) {
+                            map.put(arr[j + 1], right + 1);
+                        } else {
+                            continue;
+                        }
+
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        result++;
+                        action++;
+                    }
                 }
-            }
-            for (int j = end; j > i; j--) {
-                int left = map.getOrDefault(arr[j - 1], 0);
-                int right = map.getOrDefault(arr[j], 0);
-                if (arr[j] == q[i] && left < 2 && right < 2) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
 
-                    map.put(arr[j - 1], right + 1);
-                    map.put(arr[j], left + 1);
-                    result++;
+                for (int j = end; j > i; j--) {
+                    int left = map.getOrDefault(arr[j - 1], 0);
+                    int right = map.getOrDefault(arr[j], 0);
+                    if (arr[j] == q[i]) {
+                        if (arr[j - 1] > arr[j] && left < 2) {
+                            map.put(arr[j - 1], left + 1);
+                        } else if (arr[j - 1] < arr[j] && right < 2) {
+                            map.put(arr[j], right + 1);
+                        } else {
+                            continue;
+                        }
+
+                        int temp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = temp;
+
+                        result++;
+                        action++;
+                    }
                 }
             }
         }
