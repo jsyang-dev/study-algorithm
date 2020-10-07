46624414c6fe1b99c87b9515817a732cbda88136
Jeongsu Yang <jsyang@terafunding.com>
2020-10-07 16:54:09 +0900

    프로그래머스 - 월간 코드 챌린지 시즌1
    * 풍선 터트리기 수정

--- src/main/java/me/study/algorithm/programmers/challenge/season1/q3/Solution.java
+++ src/main/java/me/study/algorithm/programmers/challenge/season1/q3/Solution.java
@@ -1,48 +1,27 @@
 package me.study.algorithm.programmers.challenge.season1.q3;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 
 class Solution {
-    HashSet<Integer> answer = new HashSet<>();
-
     public int solution(int[] a) {
-        LinkedList<Integer> list = new LinkedList<>();
-        for (int num: a) {
-            list.add(num);
+        HashSet<Integer> answer = new HashSet<>();
+        int leftMin = 1000000000;
+        int rightMin = 1000000000;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < leftMin) {
+                answer.add(a[i]);
+                leftMin = a[i];
+            }
         }
 
-        for (int cnt = -1; cnt < list.size() - 1; cnt++) {
-            LinkedList<Integer> subList = new LinkedList<>(list);
-            process(subList, cnt);
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (a[i] < rightMin) {
+                answer.add(a[i]);
+                rightMin = a[i];
+            }
         }
 
         return answer.size();
-    }
-
-    void process(LinkedList<Integer> list, int cnt) {
-        if (list.size() == 1) {
-            answer.add(list.get(0));
-            return;
-        }
-
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (cnt == 0) {
-                if (list.get(i) < list.get(i + 1)) {
-                    list.remove(i);
-                } else {
-                    list.remove(i + 1);
-                }
-            } else {
-                if (list.get(i) > list.get(i + 1)) {
-                    list.remove(i);
-                } else {
-                    list.remove(i + 1);
-                }
-            }
-
-            LinkedList<Integer> subList = new LinkedList<>(list);
-            process(subList, cnt - 1);
-        }
     }
 }
