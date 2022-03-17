609db567e9a5e9727be4918c027d35f3a0899cd8
Jeongsu Yang <mycat83@gmail.com>
2022-03-17 19:03:07 +0900

    feat: Codility - Triangle

---
+++ src/main/java/me/study/algorithm/codility2/sorting/triangle/Solution.java
@@ -0,0 +1,24 @@
+package me.study.algorithm.codility2.sorting.triangle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+
+    public int solution(int[] A) {
+        List<Double> numbers = new ArrayList<>();
+        for (int number : A) {
+            numbers.add((double) number);
+        }
+
+        Collections.sort(numbers);
+
+        for (int i = 0; i < numbers.size() - 2; i++) {
+            if (numbers.get(i) + numbers.get(i + 1) > numbers.get(i + 2)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
